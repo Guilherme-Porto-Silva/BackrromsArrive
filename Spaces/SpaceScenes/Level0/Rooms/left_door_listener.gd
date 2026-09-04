e93957a6c73ddb5a78dc6b868407a391293e4586
Guigui-chan <guiportosilva2@gmail.com>
2026-09-03 23:46:15 -0300

    Primeiras mudança de sala e parede atravessável.

--- Spaces/SpaceScenes/Level0/Rooms/left_door_listener.gd
+++ Spaces/SpaceScenes/Level0/Rooms/left_door_listener.gd
@@ -2,6 +2,18 @@ extends Area2D
 
 const NEXT_SCENE := "res://Spaces/SpaceScenes/Level0/Rooms/full_no_clipble_front_wall_room.tscn"
 
+
+
+func _ready() -> void:
+	
+	pass
+
+
+func _process(delta: float) -> void:
+	
+	pass
+
+
 func _on_body_entered(body: Node2D) -> void:
 	
 	if body.is_in_group("players"):
